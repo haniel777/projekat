60f9021ff89d6814437ef9a07dd1689589075ab2
haniel777 <sarazdravkovic111@gmail.com>
2025-05-18 12:48:57 +0200

    Index stranica sa bootstrap i fontovima

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+
+
+
+
+
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+				<li><a class="googleplus" href="#"><span>Google+</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Sara Zdravkovic</p>
+	</div>
+</div>
